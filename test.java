b95b7f1db65881174ca94cb01d4fa57e02c456e6
abc225313 <41777892+abc225313@users.noreply.github.com>
2018-12-17 10:49:35 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 
 public class test {
 	public static void main(String[] args) {
-		String number="2895*352/263";
+		String number="2*5^3/3";
 		caculate caculate=new caculate(number);
 		caculate.process('a');
 		
